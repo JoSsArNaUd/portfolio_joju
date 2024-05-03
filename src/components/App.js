aa2ad32e757d4b30d47dfe31b51b2b1c24d93b83
Josselin Arnaud <josselin.arnaud@hefr.ch>
2024-05-03 22:19:24 +0200

    Add main page default files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
+import Main from './MainPage/Main';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World !</h1>
-      </header>
+      <Main />
     </div>
   );
 }
---
+++ src/components/MainPage/Main.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function Main() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default Main;
